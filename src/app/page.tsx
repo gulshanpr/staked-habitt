bbfe4e3536cd0573f011fbefa9dae192167c1703
gulshanpr <gulshankumarprasad04@gmail.com>
2024-12-29 03:20:10 +0530

    intmid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-// import ConnectButton from '@/components/ConnectButton';
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <h1>Welcome to Web3Modal with Wagmi</h1>
-//       <ConnectButton />
-//     </div>
-//   );
-// }
-
 "use client"
 import { useEffect, useState } from 'react';
 import supabase from '../lib/supabase';
